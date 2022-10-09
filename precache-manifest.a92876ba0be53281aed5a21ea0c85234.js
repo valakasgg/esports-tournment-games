c70d87dd5140af20505e11af8c93586fc2e19a6b
valakasgg <109357340+valakasgg@users.noreply.github.com>
2022-10-09 11:27:27 +0100

    Updates

---
+++ precache-manifest.a92876ba0be53281aed5a21ea0c85234.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fb5b4abd0a8ad18c087d99956ba09b5",
+    "url": "/esports-tournment-games/index.html"
+  },
+  {
+    "revision": "a47706bd4bd31e3a8197",
+    "url": "/esports-tournment-games/static/css/main.ed1ac773.chunk.css"
+  },
+  {
+    "revision": "11503235e6e299fbe79f",
+    "url": "/esports-tournment-games/static/js/2.995135ec.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/esports-tournment-games/static/js/2.995135ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a47706bd4bd31e3a8197",
+    "url": "/esports-tournment-games/static/js/main.c4ee29c6.chunk.js"
+  },
+  {
+    "revision": "340936fb6f103acbe5ad",
+    "url": "/esports-tournment-games/static/js/runtime-main.cab2aded.js"
+  },
+  {
+    "revision": "40d69379edc3134b3ff749b37d6707e6",
+    "url": "/esports-tournment-games/static/media/seperator.40d69379.svg"
+  }
+]);
